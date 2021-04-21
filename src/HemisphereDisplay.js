a9c166f032c4a890f68f75a4d51b5b22fa105e56
Remon Ansary <remonansary@gmail.com>
2021-04-21 10:10:45 +0600

    css update

--- src/HemisphereDisplay.js
+++ src/HemisphereDisplay.js
@@ -1,30 +1,35 @@
-import React from 'react'
-import northernPic from './image/northern.jpg'
-import southernPic from './image/southern.jpg'
- 
-const hemisphereConfig ={
-    Northern:{
-        text :'It is northern hemisphere',
-        picture:northernPic
+import React from "react";
+import "./Hemisphere.css";
+import northernPic from "./image/northern.jpg";
+import southernPic from "./image/southern.jpg";
 
-    },
-    Southern:{
-        text: 'It is southern hemisphere',
-        picture:southernPic
+const hemisphereConfig = {
+  Northern: {
+    text: "It is northern hemisphere",
+    picture: northernPic,
+  },
+  Southern: {
+    text: "It is southern hemisphere",
+    picture: southernPic,
+  },
+};
 
-    }
-}
-
-const HemisphereDisplay = ({latitude}) => {
-    //console.log(latitude)
-    const hemisphere = latitude>0 ? 'Northern' : 'Southern';
-    const {text,picture} = hemisphereConfig[hemisphere]
-    return (
-        <div className={hemisphere}>
-            <img src={picture} alt="" />
-           {text}
+const HemisphereDisplay = ({ latitude }) => {
+  //console.log(latitude)
+  const hemisphere = latitude > 0 ? "Northern" : "Southern";
+  const { text, picture } = hemisphereConfig[hemisphere];
+  return (
+    <div className={hemisphere}>
+      <div className="ui raised text container segment">
+        <div className="image">
+          <img src={picture} alt="" />
         </div>
-    )
-}
+        <div className="ui teal bottom attached label">
+          <h1> {text}</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default HemisphereDisplay
+export default HemisphereDisplay;
